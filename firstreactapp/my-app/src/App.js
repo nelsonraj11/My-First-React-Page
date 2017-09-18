40a655ac516ee1e29684d60e00989faafa6bd57a
nelsonraj11 <nelsonraj11@gmail.com>
2017-09-18 18:26:31 +0530

    second commit - Inserted logo

--- firstreactapp/my-app/src/App.js
+++ firstreactapp/my-app/src/App.js
@@ -1,24 +1,19 @@
 import React, { Component } from 'react';
-import logo1 from './logo1.png';
+import logo from './logo.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-    <div className="container">
       <div className="App">
-        <div className="App-header">
-          <img src={logo1} className="App-logo" alt="logo" />
-          <h2>Welcome to my First React page</h2>
+        <div className="container">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+          </div>
         </div>
-        <p className="App-intro">
-         
-        </p>
       </div>
-    </div>
     );
   }
 }
-
 export default App;
  
