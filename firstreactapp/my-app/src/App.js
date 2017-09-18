dc9a111b3ff2489214b25eebf9f582e344b0ee0e
nelsonraj11 <nelsonraj11@gmail.com>
2017-09-18 13:05:42 +0530

    Initial commit - My first react app

---
+++ firstreactapp/my-app/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo1 from './logo1.png';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+    <div className="container">
+      <div className="App">
+        <div className="App-header">
+          <img src={logo1} className="App-logo" alt="logo" />
+          <h2>Welcome to my First React page</h2>
+        </div>
+        <p className="App-intro">
+         
+        </p>
+      </div>
+    </div>
+    );
+  }
+}
+
+export default App;
+ 
